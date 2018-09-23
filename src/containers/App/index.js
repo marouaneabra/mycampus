f67d63511f46b519883f15c82bb040297119a54c
Minh Lam <lamchiminhmark@gmail.com>
2018-09-22 20:12:58 -0400

    wip(): working on InfoContainer

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,11 +3,15 @@
 import React from 'react';
 
 import MapCanvas from '../MapCanvas';
+import InfoContainer from '../InfoContainer';
 
 export default class App extends React.Component {
     render() {
         return (
+            <div>
             <MapCanvas />
+            <InfoContainer />
+            </div>
         );
     }
 }
---
+++ src/containers/InfoContainer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Styling
+import styled from 'styled-components';
+
+class InfoContainer extends React.Component {
+
+    render() {
+        return (
+                <div style={{display: 'inline-block', height: '100vh', width: '30wh'}}>
+                TEST
+                </div>
+        );
+    }
+}
+
+export default InfoContainer;
