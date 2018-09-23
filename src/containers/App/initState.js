705986b283508d6c71009462542bcb1335a655d3
Khang Nguyen <khang.nguyen@inapps.net>
2018-09-23 12:39:14 -0400

    Change indices

--- src/containers/App/initState.js
+++ src/containers/App/initState.js
@@ -2,7 +2,7 @@ const initState = () => {
     return ({
     "posts": [
 {
-    "id": 1,
+    "id": 0,
     "title": "Outings Club Trip to Cuyahoga National Park",
     "description": "<p>This Sunday, we will head over to Ledges Trail in Cuyahoga Valley National Park and possibly Kendall Lake Area trails. We&#39;ll head out of Oberlin from about 9am to 3pm, and picnic in between. The Ledges are an amazing spot of geologic history and you will no doubt feel like a fairy walking through the mossy structures and overlooks. Come along for a fun time with new friends and beautiful scenery.</p>\r\n\r\n<p>Fill out the lottery form here by 7 p.m. on Tuesday: <a href=\"https://docs.google.com/forms/d/e/1FAIpQLSfG6JqhMhHeXyc58oknZy9FeZ5ADb2bI5nUeCwv_OTgCUSAeQ/viewform\" target=\"_blank\">https://\
 docs.google.com/forms/d/e/1FAIpQLSfG6JqhMhHeXyc58oknZy9FeZ5ADb2bI5nUeCwv_OTgCUSAeQ/viewform</a></p>\r\n\r\n<p>Email <a href=\"mailto:ococ@oberlin.edu\" target=\"_blank\">ococ@oberlin.edu</a> for more information. This trip is sponsored by the Outings Club and the Office of Environmental Sustainability. This is an Ecolympics event!</p>\r\n",
@@ -14,7 +14,7 @@ docs.google.com/forms/d/e/1FAIpQLSfG6JqhMhHeXyc58oknZy9FeZ5ADb2bI5nUeCwv_OTgCUSA
     "end": "2018-09-23T15:00:00-04:00"
 },
 {
-    "id": 2,
+    "id": 1,
     "title": "Startup Weekend - Hosted by the Entrepreneurship Club",
     "description": "<p>Help us improve the Oberlin student experience by making the college and city more green,&nbsp;more fun,&nbsp;more accessible, more inclusive,&nbsp;more social,&nbsp;more attractive,&nbsp;more socially conscious,&nbsp;more affordable,&nbsp;more mentally or physically healthy, more global or international,&nbsp;more politically active,&nbsp;more career-ready&hellip;.. or whatever else comes to mind!&nbsp;</p>\r\n\r\n<p>Come with an idea, come just to help, come to have fun. The Oberlin College Techstars Startup Weekend is the place to look for a team, create a prototype of your idea, validate your idea \
 as sustainable, and receive feedback from experienced entrepreneurs all in one weekend.&nbsp; And: GoPro&rsquo;s, Drones, gift cards and other amazing prizes will be awarded to the teams with the best solutions.</p>\r\n\r\n<p>Sponsored by the Entrepreneurship Club</p>\r\n",
@@ -26,7 +26,7 @@ as sustainable, and receive feedback from experienced entrepreneurs all in one w
     "end": "2018-09-23T21:00:00-04:00"
 },
 {
-    "id": 3,
+    "id": 2,
     "title": "Electronic Composition Class for kids",
     "description": "<p>Learn how to build and use microphones, portable recorders, keyboards, and audio-editing software. Create cool sounds for any genre&mdash;pop, hip-hop, video game music, and more!</p>\r\n",
     "url": "www.oberlin.edu/commusic",
@@ -37,7 +37,7 @@ as sustainable, and receive feedback from experienced entrepreneurs all in one w
     "end": "2018-09-23T14:00:00-04:00"
 },
 {
-    "id": 4,
+    "id": 3,
     "title": "Lake Como Residency: Guest Master Class: William Naboré, piano ",
     "description": "<p>A guest master class by William Nabor&eacute;, piano - part of the Lake Como Residency.</p>\r\n\r\n<p>Read more about <a href=\"https://comopiano.com/faculty/\"><u>William Nabor&eacute;</u></a>.</p>\r\n\r\n<p>--</p>\r\n\r\n<p>Started in 2016, the Oberlin-Como Piano Academy is an exclusive partnership between Oberlin Conservatory and the International Piano Academy Lake Como in Italy. Each year, up to three students, selected from applicants worldwide, begin a two-year program of study at Oberlin toward an artist diploma. Called Oberlin-Como Fellows, these exemplary young musicians work with Oberlin fac\
 ulty and master artist-teachers who hold residencies at Oberlin throughout the year. All master classes and recitals presented by the visiting masters are free and open to the public. Past Oberlin-Como guest artists have included Martino Tirimo, Peter Frankl, and Paul Badura-Skoda.</p>\r\n",
@@ -49,7 +49,7 @@ ulty and master artist-teachers who hold residencies at Oberlin throughout the y
     "end": "2018-09-23T15:30:00-04:00"
 },
 {
-    "id": 5,
+    "id": 4,
     "title": "Oberlin College Field Hockey at Earlham College",
     "description": "Oberlin College Field Hockey at Earlham College\nStreaming Video: https://www.youtube.com/watch?v=MA-3BCRC9iI\n https://goyeo.com/calendar.aspx?id=4787",
     "url": "https://goyeo.com/calendar.aspx?id=4787",
@@ -60,7 +60,7 @@ ulty and master artist-teachers who hold residencies at Oberlin throughout the y
     "end": "2018-09-23T16:00:00-04:00"
 },
 {
-    "id": 6,
+    "id": 5,
     "title": "Senior Recital: Charles Ryan, trumpet",
     "description": "<p><span style=\"font-size:12pt\"><span><a href=\"http://new.oberlin.edu/conservatory/webcast/Stream_WarnerConcertHall.dot\"><span style=\"font-size:14.0pt\"><span><span><span style=\"text-decoration:none\"><span>Watch the webcast! </span></span></span></span></spnbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Charles Ryan</span></span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;\
 </span></span></span></p>\r\n\r\n<p><span style=\"font-size:12pt\"><span><span><span style=\"font-size:11.0pt\"><span>Light from a Distant Star&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&p; Mohit Dubey</span></span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; \
@@ -73,7 +73,7 @@ ulty and master artist-teachers who hold residencies at Oberlin throughout the y
     "end": null
 },
 {
-    "id": 7,
+    "id": 6,
     "title": "The Fracturing of America:  A Talk by Eliza Griswold",
     "description": "<p>Investigative reporter, translator and poet Eliza Griswold will talk about her rave-reviewed new book <i>Amity and Prosperity: One Family and the Fracturing of America,</i> which National Book Award winner Katherine Boo calls &ldquo;Riveting and outraging.&nbsp; An essential account of corporate wrongdoing, regulatory collusion, and citizen resistance in an unequal age.&rdquo; &nbsp;</p>\r\n\r\n<p>Winner of numerous prizes and fellowships, including the first Robert I. Friedman Prize in Investigative Journalism for &ldquo;In the Hiding Zone&rdquo; about Pakistan&rsquo;s Wazirstan Agency and the Anthony\
  Lukas Book Prize for <i>The Tenth Parallel</i> on the regions of the world where Christianity and Islam clash, Griswold was a 2012 Guggenheim fellow and most recently Berggruen fellow at Harvard Divinity School.</p>\r\n\r\n<p>In 2015, Griswold&#39;s translation from the Pashto of <i>I Am the Beggar of the World: Landays from Contemporary Afghanistan</i> won the PEN Award for Poetry in Translation. &nbsp;She published a book of her own poetry, <i>Wideawake Field</i>, in 2007. &nbsp;Griswold is Distinguished Writer in Residence at NYU.</p>\r\n\r\n<p>A college/community Ecolympics event; sponsors include the Office of Environmen\
@@ -86,7 +86,7 @@ tal Sustainability and the First Church Green Team.</p>\r\n",
     "end": "2018-09-23T17:30:00-04:00"
 },
 {
-    "id": 8,
+    "id": 7,
     "title": "Lake Como Residency: Guest Master Class: William Naboré, piano ",
     "description": "<p>A guest master class by William Nabor&eacute;, piano - part of the Lake Como Residency.</p>\r\n\r\n<p>Read more about <a href=\"https://comopiano.com/faculty/\"><u>William Nabor&eacute;</u></a>.</p>\r\n\r\n<p>--</p>\r\n\r\n<p>Started in 2016, the Oberlin-Como Piano Academy is an exclusive partnership between Oberlin Conservatory and the International Piano Academy Lake Como in Italy. Each year, up to three students, selected from applicants worldwide, begin a two-year program of study at Oberlin toward an artist diploma. Called Oberlin-Como Fellows, these exemplary young musicians work with Oberlin fac\
 ulty and master artist-teachers who hold residencies at Oberlin throughout the year. All master classes and recitals presented by the visiting masters are free and open to the public. Past Oberlin-Como guest artists have included Martino Tirimo, Peter Frankl, and Paul Badura-Skoda.</p>\r\n",
@@ -98,7 +98,7 @@ ulty and master artist-teachers who hold residencies at Oberlin throughout the y
     "end": "2018-09-23T19:30:00-04:00"
 },
 {
-    "id": 9,
+    "id": 8,
     "title": "Keglers Bowling League",
     "description": "<p>Interested in bowling? Keglers Intramural Bowling League is available for students and staff.</p>\r\n",
     "url": "",
@@ -109,7 +109,7 @@ ulty and master artist-teachers who hold residencies at Oberlin throughout the y
     "end": null
 },
 {
-    "id": 10,
+    "id": 9,
     "title": "Guest Recital: Sezi Seskir, fortepiano",
     "description": "<p>A conservatory guest recital performed by Sezi Seskir, fortepiano.</p>\r\n\r\n<p>Read more about <a href=\"http://www.seziseskir.com/new-page-1/\">Sezi Seskir</a>.</p>\r\n\r\n<p><i>Program:</i></p>\r\n\r\n<p class=\"ProgramText\"><span style=\"font-size:10pt\"><span><span><span style=\"font-size:11.0pt\"><span>Sonata in B-flat Major, Op. 24, No. 2&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Muzio Clementi</span></span></span></span></span></p>\r\n\r\n<p cla\
 ss=\"ProgramText\"><span style=\"font-size:10pt\"><span><span><i><span style=\"font-size:11.0pt\"><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Allegro con brio</span></span></i><span style=\"font-size:11.0pt\"><span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</span></span></span></span></span></p>\r\n\r\n<p class=\"ProgramText\"><span style=\"font-size:10pt\"><span><span><i><span style=\"font-size:11.0pt\"><span>&nbsp;&nbsp;&nbsp;\
