9928cfcef777855f8f80407019eb20a0932f25c6
Minh Lam <lamchiminhmark@gmail.com>
2018-09-23 09:13:28 -0400

    wip()

--- src/containers/InfoContainer/index.js
+++ src/containers/InfoContainer/index.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 // Styling
 import Wrapper from './Wrapper.js'
+
+import low from 'lowdb';
+import LocalStorage from 'lowdb/adapters/LocalStorage';
+
+// const low = require('lowdb')
+//     const FileSync = require('lowdb/adapters/FileSync')
+
+    // const adapter = new FileSync('db.json')
+    // const db = low(adapter)
+
+    // Set some defaults (required if your JSON file is empty)
+    //    db.defaults({ events: [], user: {}, count: 0 })
+    //.write()
+// const request = require('request');
+
+// db.defaults({ posts: [], user: {}, count: 0 })
+// 	.write()
+
+const adapter = new LocalStorage('./db.json');
+const db = low(adapter);
+
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const low = require('lowdb')
+// const FileAsync = require('lowdb/adapters/FileAsync')
+
+const result = db.get('posts').find({id:'myass'}).value();
 class InfoContainer extends React.Component {
 
     render() {
+        console.log(result)
         
         return (
             // <div style={{display: 'inline-block', position: absolute,
